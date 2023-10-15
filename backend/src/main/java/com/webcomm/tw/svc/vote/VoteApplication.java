4384538af314ac1f849a14f3fe5fdc01c877e9a1
DannyYang <yjring@gmail.com>
2023-10-15 11:20:26 +0800

    11

--- backend/src/main/java/com/webcomm/tw/svc/vote/VoteApplication.java
+++ backend/src/main/java/com/webcomm/tw/svc/vote/VoteApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @ComponentScan
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class VoteApplication {
